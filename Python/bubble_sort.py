fb6792e09c136e2a1ff074de2a6e22cf6fa05510
user <katelozanis@gmail.com>
2020-08-13 19:11:23 +0300

    20200813_19_11_upload_from_cli

---
+++ Python/bubble_sort.py
@@ -0,0 +1,12 @@
+
+#!/ usr/ bin/ python 3.6
+
+
+def InsertionSort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while (i >=0) and (A[i] > key):
+            A[i+1] = A[i]
+            i = i - 1
+        A[i+1] = key
